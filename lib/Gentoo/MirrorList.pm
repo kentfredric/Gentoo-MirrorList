65ac4eed42109be5450fb116b924779a1a00ed8a
Kent Fredric <kentfredric@gmail.com>
2009-11-09 17:07:02 +1300

    Build of 5c6c4ad, 0.01000101 on CPAN

---
+++ lib/Gentoo/MirrorList.pm
@@ -0,0 +1,474 @@
+use strict;
+use warnings;
+
+package Gentoo::MirrorList;
+our $VERSION = '0.01000101';
+
+
+
+# ABSTRACT: A programmatic way to access Gentoo.org Mirror Metadata
+
+use Moose;
+use App::Cache;
+use namespace::autoclean;
+use Gentoo::MirrorList::Mirror;
+
+
+
+has _cache => (
+  isa     => 'App::Cache',
+  is      => 'ro',
+  lazy    => 1,
+  default => sub {
+    return App::Cache->new( { ttl => 60 * 60, application => __PACKAGE__ } );
+  },
+);
+
+has '_data' => (
+  isa        => 'ArrayRef[ Gentoo::MirrorList::Mirror ]',
+  is         => 'rw',
+  lazy_build => 1,
+  traits     => [qw[ Array ]],
+  handles    => { _data_filter => 'grep', _data_iterate => 'map', _data_count => 'count', _data_shuffle => 'shuffle' },
+);
+
+has '_xml' => (
+  isa        => 'Str',
+  is         => 'ro',
+  lazy_build => 1,
+);
+
+sub _normalise_mirrorgroup {
+  my ( $self, $mirrorgroup ) = @_;
+  if ( exists $mirrorgroup->{mirror}->{uri} and exists $mirrorgroup->{mirror}->{name} ) {
+    $mirrorgroup->{mirror} = { $mirrorgroup->{mirror}->{name} => { uri => $mirrorgroup->{mirror}->{uri} } };
+  }
+  for my $name ( keys %{ $mirrorgroup->{mirror} } ) {
+    if ( ref $mirrorgroup->{mirror}->{$name}->{uri} eq 'HASH' ) {
+      $mirrorgroup->{mirror}->{$name}->{uri} = [ $mirrorgroup->{mirror}->{$name}->{uri} ];
+    }
+  }
+  return $mirrorgroup;
+}
+
+sub __build_mirrorgroup {
+  my ( $self, $mirrorgroup ) = @_;
+  $mirrorgroup = $self->_normalise_mirrorgroup($mirrorgroup);
+  my @mirrors = ();
+  my %data    = (
+    country     => $mirrorgroup->{country},
+    countryname => $mirrorgroup->{countryname},
+    region      => $mirrorgroup->{region},
+  );
+
+  for my $mirrorname ( keys %{ $mirrorgroup->{mirror} } ) {
+    for my $uri ( @{ $mirrorgroup->{mirror}->{$mirrorname}->{uri} } ) {
+      push @mirrors,
+        Gentoo::MirrorList::Mirror->new(
+        %data,
+        mirrorname => $mirrorname,
+        uri        => $uri->{content},
+        proto      => $uri->{protocol},
+        ipv4       => $uri->{ipv4},
+        ipv6       => $uri->{ipv6},
+        partial    => $uri->{partial},
+        );
+
+    }
+  }
+  return (@mirrors);
+}
+
+sub _build__data {
+  my ($self) = @_;
+
+  my $r = $self->_cache->get('data');
+  if ($r) {
+    return $r;
+  }
+  my $content = $self->_xml;
+  require XML::Simple;
+  my $structure = XML::Simple::xml_in($content);
+
+  my @rows;
+  for ( @{ $structure->{'mirrorgroup'} } ) {
+    push @rows, $self->__build_mirrorgroup($_);
+  }
+
+  $self->_cache->set( 'data', \@rows );
+  return \@rows;
+}
+
+sub _build__xml {
+  my ($self) = @_;
+  return $self->_cache->get_url('http://www.gentoo.org/main/en/mirrors3.xml');
+}
+
+sub _filter {
+  my ( $self, $property, $param ) = @_;
+  $self->_data(
+    [
+      $self->_data_filter(
+        sub {
+          return $_->property_match( $property, $param );
+        }
+      )
+    ]
+  );
+  return $self;
+}
+
+sub _unfilter {
+  my ( $self, $property, $param ) = @_;
+  $self->_data(
+    [
+      $self->s_filter(
+        sub {
+          return not $_->property_match( $property, $param );
+        }
+      )
+    ]
+  );
+  return $self;
+}
+
+
+for my $property (qw( country countryname region mirrorname uri proto ipv4 ipv6 partial )) {
+  __PACKAGE__->meta->add_method(
+    $property => sub {
+      my ( $self, $param ) = @_;
+      $self = $self->new() unless ref $self;
+      $self->_filter( $property, $param );
+      return $self;
+    }
+  );
+}
+
+
+for my $property (qw( country countryname region mirrorname uri proto )) {
+  __PACKAGE__->meta->add_method(
+    'exclude_' . $property => sub {
+      my ( $self, $param ) = @_;
+      $self = $self->new() unless ref $self;
+      $self->_unfilter( $property, $param );
+      return $self;
+    }
+  );
+}
+
+
+for my $property (qw( ipv4 ipv6 partial )) {
+  __PACKAGE__->meta->add_method(
+    'is_' . $property => sub {
+      my ( $self, $param ) = @_;
+      $self = $self->new() unless ref $self;
+      $self->_filter( $property, 1 );
+      return $self;
+    }
+  );
+  __PACKAGE__->meta->add_method(
+    'not_' . $property => sub {
+      my ( $self, $param ) = @_;
+      $self = $self->new() unless ref $self;
+      $self->_filter( $property, 1 );
+      return $self;
+    }
+  );
+}
+
+
+for my $property (qw( country countryname region mirrorname uri proto )) {
+  __PACKAGE__->meta->add_method(
+    $property . '_list' => sub {
+      my ($self) = @_;
+      $self = $self->new() unless ref $self;
+      my %v      = ();
+      my $method = Gentoo::MirrorList::Mirror->can($property);
+      $self->_data_iterate( sub { $v{ $_->$method() } = 1 } );
+      return [ sort keys %v ];
+    }
+  );
+}
+
+
+sub random {
+  my ( $self, $amt ) = @_;
+  $self = $self->new() unless ref $self;
+  $amt  = 1            unless defined $amt;
+  my (@out) = $self->_data_shuffle;
+  if ( $amt > ( $self->_data_count ) ) {
+    push @out, map { $self->_data_shuffle } 0 .. int( ( $amt - $self->_data_count ) / $self->_data_count + 1 );
+  }
+  return $out[0] if $amt == 1;
+  return @out[ 0 .. $amt ];
+}
+
+
+sub all {
+  my ($self) = @_;
+  $self = $self->new() unless ref $self;
+  return @{ $self->_data };
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Gentoo::MirrorList - A programmatic way to access Gentoo.org Mirror Metadata
+
+=head1 VERSION
+
+version 0.01000101
+
+=head1 SYNOPSIS
+
+  my @mirrors = Gentoo::MirrorList->region('North America')->country('CA')->ipv4->all;
+  my @mirrors = Gentoo::MirrorList->region('North America')->ipv4->random(3);
+  my $mirror = Gentoo::MirrorList->region('Australia')->ipv4->random();
+  my @all_names = Gentoo::MirrorList->mirrorname_list
+  my @australian_names = Gentoo::MirrorList->country('AU')->mirrorname_list;
+
+=cut
+
+=pod
+
+=head1 DESCRIPTION
+
+This module will download the most up-to-date copy of L<http://www.gentoo.org/main/en/mirrors3.xml> and parse it into
+an array of L<Gentoo::MirrorList::Mirror> which you can then filter down progressively using the methods
+defined below to find, via code, any mirror that meets a given criteria.
+
+For efficiency, this module uses L<App::Cache> to cache both the mirror list and the parsed representation of that list,
+and the data files are stored in ~/.gentoo_mirrorlist/cache/
+
+=cut
+
+=pod
+
+=head1 METHODS
+
+=head2 Explicit Filters.
+
+All of the following self-filter the data set they are on.
+
+  my $x = Gentoo::MirrorList->FILTER
+  my $y = Gentoo::MirrorList->new()
+  my $z = $y->FILTER
+
+x and y will be the same. y and z will be the same object.
+
+=head3 country
+
+  ..->country( 'AU' )->..
+  ..->country( qr/AU/ )->..
+
+See also L</country_list>
+
+=head3 countryname
+
+  ..->countryname( 'Australia' )->..
+  ..->countryname( qr/Aus/ )->..
+
+See also L</countryname_list>
+
+=head3 region
+
+  ..->region('North America')->..
+  ..->region(qr/America/)->..
+
+See also L</region_list>
+
+=head3 mirrorname
+
+  ..->mirrorname(qr/^a/i)->..
+
+See also L</mirrorname_list>
+
+=head3 uri
+
+  ..->uri(qr/gentoo/)->..
+
+See also L</uri_list>
+
+=head3 proto
+
+  ..->proto('http')->..
+  ..->proto(qr/^.*tp$/)->..
+
+See also L</proto_list>
+
+=head3 ipv4
+
+  ..->ipv4( 1 )->..
+  ..->ipv4( 0 )->..
+
+=head3 ipv6
+
+  ..->ipv6( 1 )->..
+  ..->ipv6( 0 )->..
+
+=head3 partial
+
+  ..->partial( 1 )->..
+  ..->partial( 0 )->..
+
+=cut
+
+=pod
+
+
+=head3 exclude_country
+
+  ..->exclude_country(qr/^K/i)->..
+  ..->exclude_country('AU')->..
+
+See also L</country_list>
+
+=head3 exclude_countryname
+
+  ..->exclude_countryname(qr/America/i)->..
+  ..->exclude_countryname('Australia')->..
+
+See also L</countryname_list>
+
+=head3 exclude_region
+
+  ..->exclude_region(qr/Foo/)->..
+  ..->exclude_region('Foo')->..
+
+See also L</region_list>
+
+=head3 exclude_mirrorname
+
+  ..->exclude_mirrorname(qr/Bad/)->..
+  ..->exclude_mirrorname('Bad')->..
+
+See also L</mirrorname_list>
+
+=head3 exclude_uri
+
+  ..->exclude_uri(qr/Bad\.ip/)->..
+  ..->exclude_uri('Bad.ip')->..
+
+See also L</uri_list>
+
+=head3 exclude_proto
+
+  ..->exclude_proto(qr/sync/)->..
+  ..->exclude_proto('rsync')->..
+
+See also L</proto_list>
+
+=cut
+
+=pod
+
+
+=head3 is_ipv4
+
+  ..->is_ipv4->..
+
+=head3 not_ipv4
+
+  ..->not_ipv4->..
+
+=head3 is_ipv6
+
+  ..->is_ipv6->..
+
+=head3 not_ipv6
+
+  ..->not_ipv6->..
+
+=head3 is_partial
+
+  ..->is_partial->..
+
+=head3 not_partial
+
+  ..->not_partial->..
+
+=cut
+
+=pod
+
+
+=head2 Terminating List
+
+If called directly on L<Gentoo::MirrorList> will return all data possible.
+
+If called on an object that has been filtered, only shows the data that is applicable.
+
+=head3 country_list
+
+  my ( @foo ) = ...->country_list
+
+=head3 countryname_list
+
+  my ( @foo ) = ...->countryname_list
+
+=head3 region_list
+
+  my ( @foo ) = ...->region_list
+
+=head3 mirrorname_list
+
+  my ( @foo ) = ...->mirrorname_list
+
+=head3 uri_list
+
+  my ( @foo ) = ...->uri_list
+
+=head3 proto_list
+
+  my ( @foo ) = ...->proto_list
+
+=cut
+
+=pod
+
+
+=head2 Mirror Selectors
+
+The following methods will return one or more L<Gentoo::MirrorList::Mirror> objects,
+
+They can be called directly on L<Gentoo::MirrorList> or on filtered objects.
+
+On filtered objects, the filtration that has been performed affects the output.
+
+=head3 random
+
+  my ( $mirror )  = ...->random()
+  my ( @mirrors ) = ...->random( 10 );
+
+=cut
+
+=pod
+
+
+=head3 all
+
+returns all Mirrors in the current filtration.
+
+There is no explicit sort order, but it will likely resemble parse order
+
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Kent Fredric.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Gentoo/MirrorList/Mirror.pm
@@ -0,0 +1,183 @@
+use strict;
+use warnings;
+
+package Gentoo::MirrorList::Mirror;
+our $VERSION = '0.01000101';
+
+
+
+# ABSTRACT: An objective representation of a single Gentoo mirror
+
+use Moose;
+use namespace::autoclean;
+
+my %bools = ();
+my %strs  = ();
+
+
+for (qw(  country countryname region mirrorname uri proto )) {
+  has $_ => (
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
+    traits   => [qw( String )],
+    handles  => { $_ . '_match' => 'match' },
+  );
+  $strs{$_} = 1;
+}
+
+
+for (qw( ipv4 ipv6 partial )) {
+  has $_ => ( isa => 'Bool', is => 'ro', required => 1, );
+  $bools{$_} = 1;
+}
+
+
+around BUILDARGS => sub {
+  my ( $orig, $class, @args ) = @_;
+  for my $argno ( 0 .. $#args ) {
+    last if not exists $args[ $argno + 1 ];
+    for my $bool ( keys %bools ) {
+      if ( $args[$argno] eq $bool ) {
+
+        if ( 'Y' eq uc $args[ $argno + 1 ] ) {
+          $args[ $argno + 1 ] = 1;
+        }
+        if ( 'N' eq uc $args[ $argno + 1 ] ) {
+          $args[ $argno + 1 ] = q();
+        }
+      }
+    }
+  }
+  return $class->$orig(@args);
+};
+
+
+sub property_match {
+  my ( $self, $property, $value ) = @_;
+  if ( not exists $bools{$property} and not exists $strs{$property} ) {
+    require Carp;
+    Carp::confess("Cannot match with property `$property`");
+  }
+  if ( exists $bools{$property} ) {
+    my $sub = $self->can($property);
+
+    # Xand
+    # 0 & 0 ==> 1
+    # 1 & 0 ==> 0
+    # 0 & 1 ==> 0
+    # 1 & 1 ==> 1
+    # Xand == !Xor
+    return ( not( $value xor $self->$property() ) );
+  }
+  if ( exists $strs{$property} ) {
+    my $sub = $self->can( $property . '_match' );
+    return $self->$sub($value);
+  }
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Gentoo::MirrorList::Mirror - An objective representation of a single Gentoo mirror
+
+=head1 VERSION
+
+version 0.01000101
+
+=head1 ATTRIBUTES
+
+=head2 country
+
+=head2 countryname
+
+=head2 region
+
+=head2 mirrorname
+
+=head2 uri
+
+=head2 proto
+
+=cut
+
+=pod
+
+
+=head2 ipv4
+
+=head2 ipv6
+
+=head2 partial
+
+=cut
+
+=pod
+
+=head1 METHODS
+
+=head2 country_match
+
+  ->country_match( 'str' )
+  ->country_match(qr/str/)
+
+=head2 countryname_match
+
+  ->countryname_match( 'str' )
+  ->countryname_match(qr/str/)
+
+=head2 region_match
+
+  ->region_match( 'str' )
+  ->region_match(qr/str/)
+
+=head2 mirrorname_match
+
+  ->mirrorname_match( 'str' )
+  ->mirrornamename_match(qr/str/)
+
+=head2 uri_match
+
+  ->uri_match( 'str' )
+  ->uri_match(qr/str/)
+
+=head2 proto_match
+
+  ->proto_match( 'str' )
+  ->proto_match(qr/str/)
+
+=cut
+
+=pod
+
+
+=head2 property_match
+
+A Magic Method that matches given properties
+
+  ->property_match( 'mirrorname', 'foo')
+  ->property_match( 'mirrorname', qr/foo/ )
+  ->property_match( 'ipv4', 1 )
+  ->property_match( 'ipv6', 0 )
+
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Kent Fredric.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
